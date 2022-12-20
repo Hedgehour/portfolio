83cb197054fa54bf716029b2fd90de8e155cd837
hedge <example@example.com>
2022-12-20 14:50:39 -0500

    implemented new sections for the website

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import logo from "./logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-
+import {Banner} from './components/Banner';
 import { NavBar, Navbar } from "./components/Navbar/Navbar";
-import Section from "./components/Section/Section";
-
+import {Skills} from "./components/Skill";
+import {Projects} from "./components/Projects";
+import { Contact } from "./components/Contact";
 
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner/>
+      <Skills/>
+      <Projects/>
+      <Contact/>
     </div>
   );
 }

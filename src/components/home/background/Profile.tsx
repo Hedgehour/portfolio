309755d18d260341c9ae86d38c08494a044a6c31
hedge <example@example.com>
2024-02-13 21:45:15 -0500

    Assed a new project page

--- src/components/home/background/Profile.tsx
+++ src/components/home/background/Profile.tsx
@@ -12,7 +12,7 @@ export const Profile = () => (
         )}
       >
         <svg
-          class="justify-center w-[43px] h-[43px] text-[#125767]"
+          class="justify-center w-[43px] h-[43px] text-[#365314]"
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
           fill="currentColor"
@@ -31,7 +31,7 @@ export const Profile = () => (
         )}
       >
         <svg
-          class="w-[43px] h-[43px] text-[#125767]"
+          class="w-[43px] h-[43px] text-[#365314]"
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
           fill="currentColor"
@@ -55,7 +55,7 @@ export const Profile = () => (
         )}
       >
         <svg
-          class="w-[43px] h-[43px] text-[#125767]"
+          class="w-[43px] h-[43px] text-[#365314]"
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
           fill="currentColor"
@@ -70,7 +70,7 @@ export const Profile = () => (
     </div>
     <div class="flex justify-center gap-6">
       <svg
-        class="w-8 h-8 text-[#5a6252]"
+        class="w-8 h-8 text-[#a8a29e]"
         aria-hidden="true"
         xmlns="http://www.w3.org/2000/svg"
         fill="currentColor"
@@ -79,7 +79,7 @@ export const Profile = () => (
         <path d="M10 0A10.011 10.011 0 0 0 0 10v5a3 3 0 0 0 3 3h3a1 1 0 0 0 1-1v-7a1 1 0 0 0-1-1H3c-.326.004-.65.062-.957.171a8 8 0 0 1 15.914 0A2.954 2.954 0 0 0 17 9h-3a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h3a3 3 0 0 0 3-3v-5A10.011 10.011 0 0 0 10 0Z"></path>
       </svg>
       <svg
-        class="w-8 h-8 text-[#5a6252]"
+        class="w-8 h-8 text-[#a8a29e]"
         aria-hidden="true"
         xmlns="http://www.w3.org/2000/svg"
         fill="currentColor"
@@ -88,7 +88,7 @@ export const Profile = () => (
         <path d="M19 5h-1.382l-.171-.342A2.985 2.985 0 0 0 14.764 3H9.236a2.984 2.984 0 0 0-2.683 1.658L6.382 5H5a3 3 0 0 0-3 3v11a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8a3 3 0 0 0-3-3Zm-3.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"></path>
       </svg>
       <svg
-        class="w-8 h-8 text-[#5a6252]"
+        class="w-8 h-8 text-[#a8a29e]"
         aria-hidden="true"
         xmlns="http://www.w3.org/2000/svg"
         fill="currentColor"
@@ -97,7 +97,7 @@ export const Profile = () => (
         <path d="M20.817 9.085a10 10 0 0 0-19.77 2.9A10.108 10.108 0 0 0 6.762 20a9.689 9.689 0 0 0 4.2 1h.012a3.011 3.011 0 0 0 2.144-.884A2.968 2.968 0 0 0 14 18v-.86A1.041 1.041 0 0 1 15 16h2.7a2.976 2.976 0 0 0 2.838-2.024 9.93 9.93 0 0 0 .279-4.891ZM5.5 12a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm2.707-3.793a1 1 0 1 1-1.414-1.414 1 1 0 0 1 1.414 1.414Zm2.872-1.624a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm4.128 1.624a1 1 0 1 1-1.414-1.413 1 1 0 0 1 1.414 1.413Z"></path>
       </svg>
       <svg
-        class="w-8 h-8 text-[#5a6252]"
+        class="w-8 h-8 text-[#a8a29e]"
         aria-hidden="true"
         xmlns="http://www.w3.org/2000/svg"
         fill="currentColor"
@@ -106,7 +106,7 @@ export const Profile = () => (
         <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z"></path>
       </svg>
       <svg
-        class="w-8 h-8 text-[#5a6252]"
+        class="w-8 h-8 text-[#a8a29e]"
         aria-hidden="true"
         xmlns="http://www.w3.org/2000/svg"
         fill="currentColor"
